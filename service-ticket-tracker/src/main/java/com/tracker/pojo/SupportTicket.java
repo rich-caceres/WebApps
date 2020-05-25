367deb0962e5cd953e9e8db22da150cb75f029d5
Richard Caceres <45850038+rich-caceres@users.noreply.github.com>
2020-05-25 13:28:53 -0400

    support ticket methods added.

--- service-ticket-tracker/src/main/java/com/tracker/pojo/SupportTicket.java
+++ service-ticket-tracker/src/main/java/com/tracker/pojo/SupportTicket.java
@@ -2,6 +2,37 @@ package com.tracker.pojo;
 
 public class SupportTicket {
 	
-	private enum status {Read, Completed, Sent};
+	private enum status {Read, Complete, Sent};
+	private String name;
+	private String date;
+	private String subject;
+	private String content;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getDate() {
+		return date;
+	}
+	public void setDate(String date) {
+		this.date = date;
+	}
+	public String getSubject() {
+		return subject;
+	}
+	public void setSubject(String subject) {
+		this.subject = subject;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	
+	
 
 }
