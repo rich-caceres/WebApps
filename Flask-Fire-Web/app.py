a8acb49d9ca3044d1aad99a0ea6cc95c574311c0
rich-caceres <richard.caceres22590@outlook.com>
2020-09-08 14:10:49 -0400

    added js and css to be found by flask, added functions for website navigation

--- Flask-Fire-Web/app.py
+++ Flask-Fire-Web/app.py
@@ -1,7 +1,15 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/'), method=['GET','POST']
 def index():
      return render_template("index.html")
+
+@app.route('/product'), method=['GET', 'POST']
+def product():
+     return render_template("BuyingPage.html")
+
+@app.route('/history'), method=['GET', 'POST']
+def history():
+     return render_template("History.html")
