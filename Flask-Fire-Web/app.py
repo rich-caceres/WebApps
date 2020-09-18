3bacf12e9482b6b82c9bc685e7b56d8fd8fa235d
rich-caceres <richard.caceres22590@outlook.com>
2020-09-18 11:40:02 -0400

    Updated the flask app to include database

--- Flask-Fire-Web/app.py
+++ Flask-Fire-Web/app.py
@@ -1,6 +1,13 @@
-from flask import Flask, render_template, url_for
+import os
+
+from flask import Flask, render_template, url_for, request
+from sqlalchemy import create_engine
+from sqlalchemy.orm import scoped_session, sessionmaker
 
 app = Flask(__name__)
+
+engine = create_engine(os.getenv("DATABASE_URL"))
+db = scoped_session(sessionmaker(bind=engine))
 
 @app.route('/')
 def index():
