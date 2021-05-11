394666579921e5909c954ccc017b75d48c474bc3
rich-caceres <richard.caceres22590@outlook.com>
2021-05-11 13:41:52 -0400

    adding sqlite as a database for development

--- Flask-Fire-Web/config.py
+++ Flask-Fire-Web/config.py
@@ -1,13 +1,13 @@
-#import os
+import os
 
-#basedir= os.path.abspath(os.path.dirname(__file__))
+basedir= os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
     DEBUG = False
     TESTING = False
     CSRF_ENABLED = True
     SECRET_KEY = 'SECRET_KEY'
-    #SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir,'data.sqlite')
 
 class ProductionConfig(Config):
     DEBUG = False
