357bdf695697b358654e05b5f47729498cebb2dd
rich-caceres <richard.caceres22590@outlook.com>
2021-05-15 00:17:02 -0400

    initial creation of grieve form

---
+++ Flask-Fire-Web/FireWeb/grievance/forms.py
@@ -0,0 +1,12 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
+
+class GrievanceForm(FlaskForm):
+    #TODO add fields for:
+    #entering party
+    #reason for grieve
+    #date of grieve
+    #status of grieve? this needs to be ironed out with stakeholder
+    #submit the form
+    pass
