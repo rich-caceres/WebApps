6b011b5cc36f03e75d72d8bf4d4db2e73b755bd3
Richard Caceres <richard.caceres22590@outlook.com>
2021-05-29 09:22:03 -0400

    removing imports that are not being used

--- Flask-Fire-Web/FireWeb/status/forms.py
+++ Flask-Fire-Web/FireWeb/status/forms.py
@@ -1,9 +1,8 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, DateField
+from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
 
 class StatusForm(FlaskForm):
     
     status = StringField("Status", [DataRequired(message = "Status required")])
-    date = DateField("Date")
     submit = SubmitField("Post Update")
