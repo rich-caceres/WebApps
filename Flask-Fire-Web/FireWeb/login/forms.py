4f384b635529cbd1d939b67880213b57904edde8
rich-caceres <richard.caceres22590@outlook.com>
2021-06-07 09:29:58 -0400

    Converting to email for sign in

--- Flask-Fire-Web/FireWeb/login/forms.py
+++ Flask-Fire-Web/FireWeb/login/forms.py
@@ -1,10 +1,10 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, Email
 
 class LoginForm(FlaskForm):
     
-    badge_number = StringField("Badge Number", [DataRequired(message = "Please enter badge number")])
+    email = StringField("Email", [Email(), DataRequired(message = "Please enter badge number")])
     password = StringField("Password", [DataRequired(message = "Please enter a password")])
     submit = SubmitField("Login")
   
